b9a60b9ffe5aaec945db722c74254ed860697724
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-03-18 10:33:07 -0300

    adicionando backdrop e informações

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React,{useEffect, useState} from 'react';
 import Tmdb from './Tmdb';
 import './App.css';
 
-
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+
 
 export default () => {
 
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFaturedData] = useState(null);
 
   useEffect(() =>{
     const loadAll = async () =>{
       let list = await Tmdb.getHomeList();     //pegando a lista de filmes
       setMovieList(list);
+
+      //pegando filme em destaque
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id,'tv');
+      setFaturedData(chosenInfo);
     }
 
     loadAll();
   },  []);
 
+  
   return(
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+      }
+
       <section className="lists">
         {
         movieList.map((item, key)=>(
